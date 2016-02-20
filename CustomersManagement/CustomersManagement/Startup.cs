77ca4feb6e14de100dfa18758700029c1cb473ca
HarryDD <harrysun2012@hotmail.com>
2016-02-21 10:15:59 +1100

    Creation of solution

---
+++ CustomersManagement/CustomersManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomersManagement.Startup))]
+namespace CustomersManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
