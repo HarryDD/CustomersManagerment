ed78fed116681a3ef7846294edf79cb9cbb2c085
HarryDD <harrysun2012@hotmail.com>
2016-02-24 21:52:42 +1100

    Fixed

---
+++ CustomersManagement/CustomersManagement/Controllers/CustomersController.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ToDoList.Models;
+using ToDoList.Repository;
+
+namespace ToDoList.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly CustomersRepository customersRepository = new CustomersRepository();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = customersRepository.GetAllCustomers();
+            var customersViewModel = new CustomersViewModel
+            {
+                Customers = customers,
+                SelectedCustomer = null,
+                CustomerNumber = customers == null ? 0 : customers.Count()
+            };
+            return View(customersViewModel);
+        }
+
+        public string Create()
+        {
+            return "Create New Customer";
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = customersRepository.FindCustomer(id);
+            if (customer == null)
+            {
+                ModelState.AddModelError("First", "Can't find customer with the given id");
+
+                //return RedirectToAction("Index", "Customers");
+            }
+            return View(customer);
+        }
+
+        public string Delete(int id)
+        {
+            return "Delete";
+        }
+
+        [HttpPost]
+        public ActionResult Save(
+            CustomerViewModel customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Edit", "Customers", new { customer.Id });
+            }
+
+            if (!customersRepository.UpdateCustomer(customer))
+            {
+                ModelState.AddModelError("", "Failed to update.Customer is not found!");
+            }
+
+            return RedirectToAction("Index", "Customers");
+        }
+    }
+}
---
+++ CustomersManagement/CustomersManagement/Repository/CustomersRepository.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Models;
+
+namespace ToDoList.Repository
+{
+    public class CustomersRepository
+    {
+        private static readonly List<CustomerViewModel> customers = new List<CustomerViewModel>
+        {
+            new CustomerViewModel
+            {
+                FirstName = "Cat",
+                LastName = "Shaw",
+                Address = "Macquarie",
+                Id = 1,
+                Mobile = "04888888",
+                PostalAddress = new AddressViewModel
+                {
+                    Line1 = "Macquarie",
+                    Line2 = "Macquarie",
+                    Suburb = "North Ryde",
+                    PostCode = "2000",
+                    State = "NSW"
+                },
+                ResidentialAddress = new AddressViewModel
+                {
+                    Line1 = "Macquarie11",
+                    Line2 = "Macquarie11",
+                    Suburb = "North Ryde11",
+                    PostCode = "2000",
+                    State = "NSW"
+                }
+            },
+            new CustomerViewModel
+            {
+                FirstName = "Mic",
+                LastName = "Flarit",
+                Address = "Hornsby",
+                Id = 2,
+                Mobile = "04188888",
+                PostalAddress = new AddressViewModel
+                {
+                    Line1 = "Hornsby",
+                    Line2 = "Hornsby",
+                    Suburb = "Hornsby",
+                    PostCode = "2000",
+                    State = "NSW"
+                },
+                ResidentialAddress = new AddressViewModel
+                {
+                    Line1 = "Hornsby11",
+                    Line2 = "Hornsby11",
+                    Suburb = "Hornsby11",
+                    PostCode = "2000",
+                    State = "NSW"
+                }
+            },
+            new CustomerViewModel
+            {
+                FirstName = "Mar",
+                LastName = "GVA",
+                Address = "Artarmon",
+                Id = 3,
+                Mobile = "04388888",
+                PostalAddress = new AddressViewModel
+                {
+                    Line1 = "Artarmon",
+                    Line2 = "Artarmon",
+                    Suburb = "Artarmon",
+                    PostCode = "2000",
+                    State = "NSW"
+                },
+                ResidentialAddress = new AddressViewModel
+                {
+                    Line1 = "Artarmon11",
+                    Line2 = "Artarmon11",
+                    Suburb = "Artarmon11",
+                    PostCode = "2000",
+                    State = "NSW"
+                }
+            },
+            new CustomerViewModel
+            {
+                FirstName = "Nir",
+                LastName = "Wine",
+                Address = "Straigthfield",
+                Id = 4,
+                Mobile = "04588888",
+                PostalAddress = new AddressViewModel
+                {
+                    Line1 = "Straigthfield",
+                    Line2 = "Straigthfield",
+                    Suburb = "Straigthfield",
+                    PostCode = "2000",
+                    State = "NSW"
+                },
+                ResidentialAddress = new AddressViewModel
+                {
+                    Line1 = "Straigthfield11",
+                    Line2 = "Straigthfield11",
+                    Suburb = "Straigthfield11",
+                    PostCode = "2000",
+                    State = "NSW"
+                }
+            },
+            new CustomerViewModel
+            {
+                FirstName = "DA",
+                LastName = "Pip",
+                Address = "Roseville",
+                Id = 5,
+                Mobile = "04688888",
+                PostalAddress = new AddressViewModel
+                {
+                    Line1 = "Roseville",
+                    Line2 = "Roseville",
+                    Suburb = "Roseville",
+                    PostCode = "2000",
+                    State = "NSW"
+                },
+                ResidentialAddress = new AddressViewModel
+                {
+                    Line1 = "Roseville11",
+                    Line2 = "Roseville11",
+                    Suburb = "Roseville11",
+                    PostCode = "2000",
+                    State = "NSW"
+                }
+            }
+        };
+
+        public bool AddCustomer(CustomerViewModel customer)
+        {
+            if (!IsExistingCustomer(customer))
+            {
+                customers.Add(customer);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void DeleteCustomer(int id)
+        {
+            var customer = FindCustomer(id);
+
+            if (customer != null)
+            {
+                customers.Remove(customer);
+            }
+        }
+
+        public bool IsExistingCustomer(CustomerViewModel customer)
+        {
+            return customer != null && FindCustomer(customer.Id) != null;
+        }
+
+        public bool UpdateCustomer(CustomerViewModel customer)
+        {
+            if (customer == null)
+            {
+                return false;
+            }
+
+            var existingCustomer = FindCustomer(customer.Id);
+
+            if (existingCustomer != null)
+            {
+                DeleteCustomer(customer.Id);
+            }
+
+            customers.Add(customer);
+
+            return true;
+        }
+
+        public CustomerViewModel FindCustomer(int id)
+        {
+            return customers.SingleOrDefault(c => c.Id == id);
+        }
+
+        public IEnumerable<CustomerViewModel> GetAllCustomers()
+        {
+            return customers;
+        }
+    }
+}
